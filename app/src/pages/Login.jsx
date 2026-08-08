c44673d0525f0a4a77ff0774215037f95676ab1d
abraaosantosdev <abraaofilipi12@gmail.com>
2026-08-08 13:29:10 -0300

    feat: implementa tela de login com campos controlados e navegação

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import AuthButton from "../components/buttons/AuthButton"
+import AuthInput from "../components/inputs/AuthInput"
+import logo from '../assets/icons/logo_text.svg'
+import { Link } from 'react-router-dom'
+import './Login.css'
+
 function Login() {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
     return (
-        <div>
-            <h1>Esta é a página de login</h1>
+        <div className="login-form">
+
+            <img src={logo} alt="Logo Blabry" className="logo" />
+            <div className="input-wrapper">
+                <AuthInput label={"Email"} placeholder={"email@exemplo.com"} fieldType={"Email"} fieldId={"email-input"} value={email} onChange={(e) => setEmail(e.target.value)} />
+                <AuthInput label={"Senha"} placeholder={"Senha"} fieldType={"Password"} fieldId={"password-input"} value={senha} onChange={(e) => setSenha(e.target.value)} />
+                <Link className="forgot-psw" to="">Esqueci a senha</Link>
+            </div>
+
+            <AuthButton label={"Login"} />
+            <p className="already-user">Não tem conta <Link className="already-user-link" to="/novaconta">Cadastre-se</Link></p>
         </div>
     )
 }
