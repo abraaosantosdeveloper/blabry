a43712c27d8135aac586596c0d46370fba62a825
abraaosantosdev <abraaofilipi12@gmail.com>
2026-08-23 22:13:47 -0300

    Implementado toast: v1

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,40 +1,37 @@
 import { useState } from "react"
+import { Link, useNavigate } from 'react-router-dom'
+import { login } from '../services/auth.service'
 import AuthButton from "../components/buttons/AuthButton"
 import AuthInput from "../components/inputs/AuthInput"
 import Toast from "../components/toasts/Toast"
 import logo from '../assets/icons/logo_text.svg'
-import { Link, useNavigate } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
-    // const [toast, setToast] = useState({ mensagem: 'Login realizado!', tipo: 'sucesso', visible: true })
+    const [toast, setToast] = useState({ mensagem: '', tipo: '', visible: false })
     const navigate = useNavigate()
 
     async function handleLogin() {
-        const resposta = await fetch('https://pretty-generosity-production-b8c6.up.railway.app/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email: email, senha: senha })
-        })
-
-        const dados = await resposta.json()
-        console.log(dados);
-
+        const dados = await login({ email, senha })
         if (dados.token) {
             localStorage.setItem('token', dados.token)
             localStorage.setItem('nome', dados.usuario.nome)
-            navigate('/feed')
+            setToast({ mensagem: 'Login realizado!', tipo: 'sucesso', visible: true })
+            setTimeout(() => navigate('/feed'), 1500)
         }
+        else {
+            setToast({ mensagem: 'Erro no login...', tipo: 'erro', visible: true })
+
+        }
+
     }
 
     return (
         <div className="login-form">
 
-            {/* <Toast mensagem={toast.mensagem} tipo={toast.tipo} visible={toast.visible} /> */}
+            <Toast mensagem={toast.mensagem} tipo={toast.tipo} visible={toast.visible} />
 
             <img src={logo} alt="Logo Blabry" className="logo" />
             <div className="input-wrapper">
